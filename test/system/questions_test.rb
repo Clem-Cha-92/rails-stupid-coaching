131b77033ee959f12996a7dd55710e180d53b1c4
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-09 12:40:12 +0200

    Completed Stupid Coaching challenge

---
+++ test/system/questions_test.rb
@@ -0,0 +1,24 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "asking a question yields a sharp response from the coach" do
+    take_screenshot
+    visit ask_url
+    fill_in "question", with: "How are you?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+
+
+end
